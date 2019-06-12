59968b414a3d0141d11e6c829b75786b3875cb93
sujeetimpact <sujeet.yadav@impactguru.com>
2019-06-12 10:25:14 +0530

    Updates

---
+++ precache-manifest.ea3aad332172a1d50029b442b346c5f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3ee1326d79a709ef150337b37e830b",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c7af4ee34838298f7c37",
+    "url": "/todoapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "af257e9704e6c045b638",
+    "url": "/todoapp/static/css/main.f27c22ef.chunk.css"
+  },
+  {
+    "revision": "c7af4ee34838298f7c37",
+    "url": "/todoapp/static/js/2.f35093c9.chunk.js"
+  },
+  {
+    "revision": "af257e9704e6c045b638",
+    "url": "/todoapp/static/js/main.5c053534.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
