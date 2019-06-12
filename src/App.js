c815a19edb98ff88153db52219ed92e65c0fc4f0
sujeetimpact <sujeet.yadav@impactguru.com>
2019-06-12 10:11:04 +0530

    todolist complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
-import React from 'react'
-function App(){
-    return <h1>Hello React</h1>
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+class App extends React.Component {
+  state = {
+    todoList: [],
+    name: "",
+    todo: ""
+  };
+  addTodo = this.addTodo.bind(this);
+  handleinput = event => {
+    this.setState({ todo: event.target.value });
+  };
+
+  addTodo() {
+    this.setState({
+      todoList: [...this.state.todoList, this.state.todo],
+      todo: ""
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.todo}</h1>
+        <input
+          type="text"
+          value={this.state.todo}
+          onChange={this.handleinput}
+        />
+        <button onClick={this.addTodo}>Add</button>
+
+        <ul>
+          {this.state.todoList.map(function(item, index) {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+        {/* <ul>
+          {this.state.todo.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul> */}
+      </div>
+    );
+  }
 }
 
 export default App;
